c3b89a91627add0329b9325083354887517de7a5
Dawid24 <paciorekdawidonly@gmail.com>
2018-09-30 12:37:59 +0200

    first commit

---
+++ src/main/java/pl/akademiakodu/generateForm/GenerateFormApplication.java
@@ -0,0 +1,12 @@
+package pl.akademiakodu.generateForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerateFormApplication.class, args);
+	}
+}
